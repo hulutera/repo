e0a6ad94865d31e4b3d61181dc143a94e8daef7b
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-01-24 18:42:08 +0100

    moved global.variable.php  to includes/

--- classes/cmn.class.php
+++ classes/cmn.class.php
@@ -1,6 +1,6 @@
 <?php
 $documnetRootPath = $_SERVER['DOCUMENT_ROOT'];
-require_once $documnetRootPath.'/classes/global.variable.php';
+require_once $documnetRootPath.'/includes/global.variable.php';
 require_once $documnetRootPath.'/classes/image.class.php';
 require_once $documnetRootPath.'/classes/path.class.php';
 require_once $documnetRootPath.'/classes/user.class.php';
