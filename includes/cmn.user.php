a6a9fb0a0690c4591f35c7af250db04d9ba84fb1
wendeworku <wendeworku@gmail.com>
2020-02-02 10:44:58 +0100

    Revert "More updates" to fix merge conflict
    This reverts commit f5b8d30e3d4c494a5eb7f10d374e4e36ade4a889.

--- includes/cmn.user.php
+++ includes/cmn.user.php
@@ -2,58 +2,54 @@
 session_start();
 $documnetRootPath = $_SERVER['DOCUMENT_ROOT'];
 require_once $documnetRootPath.'/db/database.class.php';
-require_once $documnetRootPath.'/includes/global.variable.php';
+require_once $documnetRootPath.'/classes/global.variable.class.php';
 
 function userLogin($email, $password)
 {
     $connect = DatabaseClass::getInstance()->getConnection();
-    
+
     if(empty($email) && empty($password)) {
-        return ERR_ENTER_EMAIL_AND_PASS;
+        return htGlobal::get('ERR_ENTER_EMAIL_AND_PASS');
     }
     else if (empty($email) || empty($password)) {
         if (empty($password)) {
-            return ERR_ENTER_PASS;
+            return htGlobal::get('ERR_ENTER_PASS');
         }
         else {
-            return ERR_ENTER_EMAIL;                
+            return htGlobal::get('ERR_ENTER_EMAIL');                
         }
     }
     else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return ERR_INVALID_EMAIL;
+        return htGlobal::get('ERR_INVALID_EMAIL');
     }
     else {
         $email_1 = $connect->real_escape_string($email);
         $password_1 = $connect->real_escape_string($password);
         $successLogin = FALSE;
-		$table = "user";
-	    $filterId = "*";
-	    $condition = "WHERE uEmail = '$email_1'";
-	    $result = DatabaseClass::getInstance()->findTotalItemNumb($filterId, $table, $condition);
+        $result = $connect->query("SELECT * FROM user WHERE uEmail = '$email_1'");
         if (mysqli_num_rows($result) != 0) {
             $row = $result->fetch_array();
-            if (crypt($password_1, $row['uNewPassword']) == $row['uPassword']){
+            if (crypt($password_1, $row['uPassword']) == $row['uPassword']){
                 $successLogin = TRUE;
             }
             else if($row['activation'] != NULL) { //Allow login before activation for recovered password    
                 if (crypt($password_1, $row['uNewPassword']) == $row['uNewPassword']) {
                     $newPassword=$row['uNewPassword'];
-					$sql = "UPDATE user SET upassword = '$newPassword', uNewPassword = NULL, activation= NULL WHERE uEmail = '$email'";
-					$result2 = DatabaseClass::getInstance()->runQuery($sql);
+                    $result2=$connect->query("UPDATE user SET upassword = '$newPassword', uNewPassword = NULL, activation= NULL WHERE uEmail = '$email'") or die (mysqli_error());
                     $successLogin = TRUE;
                 }
             }
             if($successLogin == TRUE) {
                 $_SESSION['uID'] = $row['uID'];
                 $_SESSION['time'] = time();
-                return LOGIN_SUCCESS;                
+                return htGlobal::get('LOGIN_SUCCESS');                
             }
             else {
-                return ERR_WORNG_EMAIL_OR_PASS;
+                return htGlobal::get('ERR_WORNG_EMAIL_OR_PASS');
             }
         }
         else {
-            return ERR_USER_NOT_EXIST;
+            return htGlobal::get('ERR_USER_NOT_EXIST');
         }
     }
 }
