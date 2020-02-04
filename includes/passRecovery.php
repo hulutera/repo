2f8551f4a85f2f891ff0ef6b6767450989bbfc3c
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-02-04 23:17:56 +0100

    broken link fixred in passRecovery

--- includes/passRecovery.php
+++ includes/passRecovery.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $documnetRootPath = $_SERVER['DOCUMENT_ROOT'];
-require_once $documnetRootPath.'/includes/item.inc.php';
+require_once $documnetRootPath.'/includes/headerSearchAndFooter.php';
 
 global $connect;
 $error_message="";
