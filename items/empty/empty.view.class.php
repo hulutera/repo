ea1d557326b1409b88d42494eaf4f3bd4ca09013
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-04-20 22:30:58 +0200

    cleanup old upload related classes

--- items/empty/empty.view.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class EmptyView
-{
-    public function show($item)
-    {
-        echo "<div id='mainColumnX'> <div id='spanMainColumnX'>" . htGlobal::get('MESSAGE_TYPE_ITEM_NOT_FOUND') . "</div></div>";
-    }
-}
