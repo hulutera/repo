ea365b50700f55d46091a40d8eedc5e776f8845e
dochoex@gmail.com <abiy.terefe@hotmail.com>
2020-08-09 22:58:46 +0200

    logout.php when accessed directly to route to index.php

--- includes/logout.php
+++ includes/logout.php
@@ -3,18 +3,26 @@ ob_start();
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
+if (isset($_GET['lan'])) {
+    $lang_url = "?&lan=" . $_GET['lan'];
+} else {
+    $lang_url = "?&lan=en";
+}
+
 if (isset($_SESSION['uID'])) {
-    //$documnetRootPath = $_SERVER['DOCUMENT_ROOT'];
-    //require_once $documnetRootPath . '/db/database.class.php';
-    $http_referer = $_SERVER['HTTP_REFERER'];
+    ///
     session_destroy();
+    ///
     session_unset();
+    ///
     $_SESSION = array();
-    if (isset($_GET['lan'])) {
-        $lang_url = "?&lan=" . $_GET['lan'];
-    } else {
-        $lang_url = "";
-    }
+    ///
+    header("Location:../index.php$lang_url");
+}
+
+/// logout.php accessed directly will be routed to index.php
+if(!isset($_SERVER['HTTP_REFERER']))
+{
     header("Location:../index.php$lang_url");
 }
 ob_end_flush();
